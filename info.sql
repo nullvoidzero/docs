805604e81f7a188efc37f9af45898f06668e8363
nullvoidzero <nullvoid@naver.com>
2024-02-15 08:33:38 +0900

    add info.sql

---
+++ info.sql
@@ -0,0 +1,23 @@
+
+nullvoid@naver.com 02577Min@
+https://github.com/nullvoidzero/docs.git
+
+user.name=nullvoidzero
+user.email=nullvoid@naver.com
+
+-- ...or create a new repository on the command line
+echo "# Document" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/nullvoidzero/Document.git
+git push -u origin master
+
+
+-- ...or push an existing repository from the command line
+git remote add origin https://github.com/nullvoidzero/Document.git
+git branch -M master
+git push -u origin master
+
+
